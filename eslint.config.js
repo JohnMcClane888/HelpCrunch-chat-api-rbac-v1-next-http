c4deb1e1d67ff41e9f4479bfe0bac03059b4b0c3
John McClane <168bvv@proton.me>
2026-08-08 22:05:59 +0300

    ignore underscore unused vars in eslint

--- eslint.config.js
+++ eslint.config.js
@@ -21,7 +21,14 @@ export default tseslint.config(
 
     rules: {
       '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/no-unused-vars': 'warn',
+
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+        },
+      ],
     },
   },
 );
